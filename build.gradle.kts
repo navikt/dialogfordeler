dec19827ee34a3cb507b76510ef9533257401f52
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-01-20 11:44:18 +0100

    Delete redundant dependency javax.inject
    Inject is already included via Spring Boot

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,6 @@ repositories {
 }
 
 dependencies {
-    implementation("javax.inject:javax.inject:1")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-jta-atomikos")
